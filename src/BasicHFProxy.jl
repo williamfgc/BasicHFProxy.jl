0415609eb896d7fbcad02e11799cd7b4b08cf413
Carsten Bauer <crstnbr@gmail.com>
2023-03-23 17:29:13 +0100

    draft (running but gives incorrect result)

--- src/BasicHFProxy.jl
+++ src/BasicHFProxy.jl
@@ -2,6 +2,7 @@ module BasicHFProxy
 
 import SpecialFunctions # erf
 import Atomix
+import FLoops
 
 function datafilter(fname)
     contains(fname, '_') && return false
@@ -17,7 +18,11 @@ include("common.jl")
 include("sequential.jl")
 include("threads_lock.jl")
 include("threads_atomix.jl")
+include("threads_floops.jl")
 
-export bhfp_sequential, bhfp_threads_lock, bhfp_threads_atomix
+export bhfp_sequential,
+       bhfp_threads_lock,
+       bhfp_threads_atomix,
+       bhfp_threads_floops
 
 end
